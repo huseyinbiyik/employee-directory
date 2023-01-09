582603b2271f8529eb3ccdf3e41ba82d3857e6ba
Huseyin Biyik <25887253+huseyinbiyik@users.noreply.github.com>
2023-01-09 13:56:35 +0300

    Add react pagination to list employees

--- app/javascript/components/Employees.js
+++ app/javascript/components/Employees.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { useEffect } from "react";
+import Pagination from "react-paginate";
+import { Link } from "react-router-dom";
 
 const Employees = () => {
-  return <p>hi</p>;
+  const [employees, setEmployees] = React.useState([]);
+  const [pageCount, setPageCount] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(0);
+
+  useEffect(() => {
+    fetch(`https://reqres.in/api/users?page=${currentPage + 1}`, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setEmployees(response.data);
+        setPageCount(response.total_pages);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [currentPage]);
+
+  const handlePageClick = (data) => {
+    let selected = data.selected;
+    setCurrentPage(selected);
+  };
+
+  return (
+    <div>
+      <Pagination
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        forcePage={currentPage}
+      />
+      {employees.map((employee) => (
+        <div key={employee.id}>
+          <Link to={`employee/${employee.employee_id}`}>
+            <p>
+              {employee.first_name} {employee.last_name}
+            </p>
+            <p>{employee.email}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Employees;
