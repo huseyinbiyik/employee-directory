41ffa7c8e037945859948681e90af7f3dc1eacd9
Huseyin Biyik <25887253+huseyinbiyik@users.noreply.github.com>
2023-01-09 11:48:36 +0300

    Fetch data from reqres

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -2,6 +2,19 @@ module Api
   module V1
     class EmployeesController < ApplicationController
       def index
+        response = HTTParty.get('https://reqres.in/api/users')
+        response_body = response.body
+        parsed_response = JSON.parse(response_body)
+        Employee.delete_all
+        parsed_response['data'].each do |e|
+          Employee.create(
+            employee_id: e['id'],
+            first_name: e['first_name'],
+            last_name: e['last_name'],
+            email: e['email'],
+            avatar: e['avatar']
+          )
+        end
         @employees = Employee.all
         render json: @employees
       end
