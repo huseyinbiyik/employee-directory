82aafe7ee781c2715c6d19a4bab666aab670c1bd
Huseyin Biyik <25887253+huseyinbiyik@users.noreply.github.com>
2023-01-09 19:02:14 +0300

    Refactor employees controller

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -2,46 +2,34 @@ module Api
   module V1
     class EmployeesController < ApplicationController
       def index
-        response = HTTParty.get("https://reqres.in/api/users?page=#{params[:page]}")
-        response_body = response.body
+        response_body = HttpHelper.get_employee_list(params[:page])
         parsed_response = JSON.parse(response_body)
-        Employee.delete_all
-        parsed_response['data'].each do |e|
-          Employee.create(
-            employee_id: e['id'],
-            first_name: e['first_name'],
-            last_name: e['last_name'],
-            email: e['email'],
-            avatar: e['avatar']
-          )
-        end
+        create_employee_records(parsed_response['data'])
         @employees = Employee.all
         render json: { status: 200, data: @employees, total_pages: parsed_response['total_pages'] }
       end
 
       def show
-        response = HTTParty.get("https://reqres.in/api/users/#{params[:id]}")
-        response_body = response.body
+        response_body = HttpHelper.get_employee(params[:id])
         parsed_response = JSON.parse(response_body)
-        Employee.delete_all
-        parsed_response = parsed_response['data']
-        @employee = Employee.create(
-          employee_id: parsed_response['id'],
-          first_name: parsed_response['first_name'],
-          last_name: parsed_response['last_name'],
-          email: parsed_response['email'],
-          avatar: parsed_response['avatar']
-        )
-
+        create_employee_record(parsed_response['data'])
+        @employee = Employee.last
         render json: { status: 200, employee: @employee }
       end
 
       def search
-        response = HTTParty.get('https://reqres.in/api/users?per_page=12')
-        response_body = response.body
+        response_body = HttpHelper.search_employees
         parsed_response = JSON.parse(response_body)
+        create_employee_records(parsed_response['data'])
+        @employees = Employee.where('email LIKE ?', "%#{params[:email]}%")
+        render json: { status: 200, data: @employees }
+      end
+
+      private
+
+      def create_employee_records(data)
         Employee.delete_all
-        parsed_response['data'].each do |e|
+        data.each do |e|
           Employee.create(
             employee_id: e['id'],
             first_name: e['first_name'],
@@ -50,8 +38,17 @@ module Api
             avatar: e['avatar']
           )
         end
-        @employees = Employee.where('email LIKE ?', "%#{params[:email]}%")
-        render json: { status: 200, data: @employees }
+      end
+
+      def create_employee_record(data)
+        Employee.delete_all
+        Employee.create(
+          employee_id: data['id'],
+          first_name: data['first_name'],
+          last_name: data['last_name'],
+          email: data['email'],
+          avatar: data['avatar']
+        )
       end
     end
   end
